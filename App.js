656fa5a1171c5c66171f9f9c831abce38f4f6363
medobarakat <ahmedbarakat2401@gmail.com>
2022-07-23 10:00:00 +0200

    add screen

--- App.js
+++ App.js
@@ -2,19 +2,36 @@ import { Text, View } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home!1</Text>
-    </View>
-  );
-}
+import Home from "./src/Screens/Home";
 
 function SettingsScreen() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Settings!</Text>
+    </View>
+  );
+}
+
+function AddScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>AddScreen!</Text>
+    </View>
+  );
+}
+
+function SpecialScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>SpecialScreen!</Text>
+    </View>
+  );
+}
+
+function OrderScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>OrderScreen!</Text>
     </View>
   );
 }
@@ -26,14 +43,35 @@ export default function App() {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ tabBarIcon: makeIconRender("home") }}
-        />
-        <Tab.Screen
           name="Settings"
           component={SettingsScreen}
           options={{ tabBarIcon: makeIconRender("cog") }}
+        />
+        <Tab.Screen
+          name="add"
+          component={AddScreen}
+          options={{ tabBarIcon: makeIconRender("plus") }}
+        />
+        <Tab.Screen
+          name="special"
+          component={SpecialScreen}
+          options={{ tabBarIcon: makeIconRender("plus") }}
+        />
+        <Tab.Screen
+          name="orders"
+          component={OrderScreen}
+          options={{ tabBarIcon: makeIconRender("offer") }}
+        />
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: makeIconRender("home"),
+            title: "الرئيسيه",
+            headerTitle: "الرئيسيه",
+            headerTitleAlign: "center",
+            headerTintColor: "blue",
+          }}
         />
       </Tab.Navigator>
     </NavigationContainer>
